371d3fa313d84ce3ed0cb740816e73547f8f1fc9
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2015-10-25 19:56:25 +0100

    Reformatted code.

--- main.c
+++ main.c
@@ -11,28 +11,29 @@
  * Returns int provided by comparison function.
  *
  */
-int lexicographicalArrayCompare (void * arr1, void * arr2, int elemSize, int size1, int size2, int (*compare) (void *, void *)) {
+int lexicographicalArrayCompare(void *arr1, void *arr2, int elemSize, int size1, int size2,
+                                int (*compare)(void *, void *)) {
 
-	// compare first element of array with comparator function
-	int result = compare(arr1, arr2);
+    // compare first element of array with comparator function
+    int result = compare(arr1, arr2);
 
     // in case the first elements do not match, check the next elements
     // in the array, if they exist
-	if (result == 0 && size1 != 0 && size2 != 0) {
+    if (result == 0 && size1 != 0 && size2 != 0) {
 
-		// decrease the size of each array
-		size1--;
-		size2--;
+        // decrease the size of each array
+        size1--;
+        size2--;
 
-		// increment pointers to compare the next element
-		void * arr1Addr = (char *) arr1 + elemSize;
-		void * arr2Addr = (char *) arr2 + elemSize;
+        // increment pointers to compare the next element
+        void *arr1Addr = (char *) arr1 + elemSize;
+        void *arr2Addr = (char *) arr2 + elemSize;
 
         assert(arr1Addr != NULL);
         assert(arr2Addr != NULL);
 
         // check the next element
-		return lexicographicalArrayCompare(arr1Addr, arr2Addr, elemSize, size1, size2, compare);
+        return lexicographicalArrayCompare(arr1Addr, arr2Addr, elemSize, size1, size2, compare);
 
     } else {
 
@@ -43,27 +44,27 @@ int lexicographicalArrayCompare (void * arr1, void * arr2, int elemSize, int siz
 /**
  * Comparison Function for double
  */
-int compareDouble (void * pn, void * pm) {
+int compareDouble(void *pn, void *pm) {
     double n = *(double *) pn;
     double m = *(double *) pm;
 
     printf("comparing n = %f, m = %f\n", n, m);
 
-	if (n < m) {
-		return -1;
-	} else if (n > m) {
-		return 1;
-	} else {
-		return 0;
-	}
+    if (n < m) {
+        return -1;
+    } else if (n > m) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 /**
  * Comparison Function for char
  */
-int compareChar (void * pn, void * pm) {
-    char * n = (char *) pn;
-    char * m = (char *) pm;
+int compareChar(void *pn, void *pm) {
+    char *n = (char *) pn;
+    char *m = (char *) pm;
 
     return memcmp(n, m, sizeof(char));
 }
